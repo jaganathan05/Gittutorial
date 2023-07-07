2161637588edf603261c88125469020c3d59d668
jaganathan05 <jaganathanv888@gmail.com>
2023-07-07 10:18:11 +0530

    New-app

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 console.log("hello world!");
-console.log(document.forms);
+var items=document.getElementsByClassName("list-group-item");
+//console.log(items[2]);
+items[2].style.backgroundColor="green"
+for(let i=0; i<=items.length-1 ; i++){
+    items[i].style.fontWeight='bold'
+}
+
