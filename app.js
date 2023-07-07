bbb50f253fe52bc5e7948b6c3f955ffc2fbc7dd9
jaganathan05 <jaganathanv888@gmail.com>
2023-07-07 11:17:29 +0530

    upadte for class name & Tag name

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ for(let i=0; i<=items.length-1 ; i++){
     items[i].style.fontWeight='bold'
 }
 
+console.log(document.getElementsByClassName("list-group-item"))
+var tag_of_items=document.getElementsByTagName("li") ;
+console.log(tag_of_items);
+for (let i=0;i<=tag_of_items.length-1;i++){
+    tag_of_items[i].style.fontSize='30px' 
+    tag_of_items[i].style.color="orange"
+}
