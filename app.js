14559162733f7eb8109ab87e00a4740d04a60fef
jaganathan05 <jaganathanv888@gmail.com>
2023-07-08 15:46:06 +0530

    Delete data from Local Storage

--- app.js
+++ app.js
@@ -1,50 +1,64 @@
 function getacall(event) {
   event.preventDefault();
-  var name=document.getElementById('name').value 
-  var email=document.getElementById('email').value
-  var PhoneNumber=document.getElementById('tel').value
-  var date=document.getElementById('date').value
-  var time=document.getElementById('time').value
 
-  // // store user details in local storage 
-  // localStorage.setItem('name',name)
-  // localStorage.setItem('email',email)
-  // localStorage.setItem('PhoneNumber',PhoneNumber)
-  // localStorage.setItem('date',date)
-  // localStorage.setItem('time',time)  
-  
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var phoneNumber = document.getElementById('tel').value;
+  var date = document.getElementById('date').value;
+  var time = document.getElementById('time').value;
 
-  // create a object for details 
   var userDetails = {
-    name: name,
-    email: email,
-    phoneNumber: PhoneNumber,
-    date: date,
-    time: time
+      name: name,
+      email: email,
+      phoneNumber: phoneNumber,
+      date: date,
+      time: time
   };
-  // Store user details object in local storage
-  localStorage.setItem('user', JSON.stringify(userDetails));
-  //console.log(localStorage);
 
   // Retrieve existing user details from local storage
-  var existingDetails = localStorage.getItem('userDetails');
+  var existingDetails = JSON.parse(localStorage.getItem('userDetails'));
 
   if (existingDetails) {
+      // If there are existing user details, append the new user details
+      existingDetails.push(userDetails);
+      localStorage.setItem('userDetails', JSON.stringify(existingDetails));
+  } else {
+      // If there are no existing user details, create a new array with the current user details
+      var newDetails = [userDetails];
+      localStorage.setItem('userDetails', JSON.stringify(newDetails));
+  }
 
-  // If there are existing user details, parse and update the array
-  existingDetails = JSON.parse(existingDetails);
-  existingDetails.push(userDetails);
-  localStorage.setItem('userDetails', JSON.stringify(existingDetails));
-  
-} else {
+  // Display the user details on the web page
+  var userDetailsContainer = document.createElement("div");
+  userDetailsContainer.innerHTML = "<h3>User Details:</h3>" +
+      "<p>Name: " + name + "</p>" +
+      "<p>Email: " + email + "</p>" +
+      "<p>Phone Number: " + phoneNumber + "</p>" +
+      "<p>Time for Call: " + date + " " + time + "</p>";
 
-  // If there are no existing user details, create a new array with the current user details
-  var newDetails = [userDetails];
-  localStorage.setItem('userDetails', JSON.stringify(newDetails));
-  
-}
+  // Create a delete button
+  var deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", function () {
+      userDetailsContainer.remove(); // Remove the userDetailsContainer from the DOM
 
-  // Retrieve and log the stored user details
-  var storedUser = JSON.parse(localStorage.getItem('user'));
-  console.log(storedUser);
+      // Retrieve existing user details from local storage
+      var storedDetails = JSON.parse(localStorage.getItem('userDetails'));
+
+      // Find the index of the user details to be deleted
+      var index = storedDetails.findIndex(function (detail) {
+          return detail.name === name && detail.email === email && detail.phoneNumber === phoneNumber;
+      });
+
+      if (index !== -1) {
+          // Remove the user details from the stored array
+          storedDetails.splice(index, 1);
+          localStorage.setItem('userDetails', JSON.stringify(storedDetails));
+      }
+  });
+
+  // Append the delete button to the userDetailsContainer
+  userDetailsContainer.appendChild(deleteButton);
+
+  document.body.appendChild(userDetailsContainer);
 }
